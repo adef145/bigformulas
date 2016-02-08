3ece2d59438c6e7f76a1f3eb6ee28e4871ff1239
Ade Fruandta <ade.fruandta@gmail.com>
2016-02-08 11:22:44 +0700

    add command

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 'use strict';
 
 var collection = {
+  // statistic
   mean: require('./lib/statistic/mean'),
   median: require('./lib/statistic/median'),
   modus: require('./lib/statistic/modus'),
   max: require('./lib/statistic/max'),
   maxDiff: require('./lib/statistic/maxDiff'),
   min: require('./lib/statistic/min'),
+  // matrix
   transpose: require('./lib/matrix/transpose'),
+  // collection
   lookup: require('./lib/collection/lookup'),
+  // datetime
   dateAdd: require('./lib/datetime/dateAdd'),
   dateDiff: require('./lib/datetime/dateDiff'),
+  // converter
   convTemperature: require('./lib/converter/convTemperature')
 };
 
